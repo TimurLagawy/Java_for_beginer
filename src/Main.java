069bbe9e3e7e755abd254da1c3cdd196cdb1a22b
TimurLagawy <timurtagirovich@mail.ru>
2022-11-28 14:50:07 +0100

    28_11_22

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.net.SocketTimeoutException;
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class Main {
+    public static void main(String[] args) {
+
+       /* System.out.println("Hello world!");
+        System.out.print("Hello ");
+        System.out.print("wor\n \t" +
+                "ld");
+    }
+}
+
+        */
+            for (int i = 0; i < 10; i++) {
+            System.out.println(i + ". ");
+        }
+    }
+}
