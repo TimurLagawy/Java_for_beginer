6b06d2eaf12fcd75e59b7c12dedb580ff299fdb3
Timur Lagawy <timurtagirovich@mail.ru>
2022-12-01 22:11:47 +0100

    01_12_22

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.net.SocketTimeoutException;
 import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,13 +23,23 @@ public class Main {
 Есть сотрудник, назовем его NNNN(создаем переменную, задаем имя), он получает в месяц 400 рублей аванса и 800 рублей зарплаты.
 Также он получает премию раз в квартал в размере 20 процентов от месячного дохода.
 Необходимо подсчитать годовой доход сотрудника и вывести в консоль результат подсчета в формате «NNNN получает ХХХХ рублей в год.»
-*/ int NNNN;
+*//* int NNNN;
 int pre_sal=400;
 int sal=800;
 float premium=0.2f;
         {
             System.out.println("NNNN получает " + ((pre_sal + sal) * 12 + (pre_sal + sal) * 4 * premium) + "рублей в год.");
+*///ввод данных
+        Scanner in=new Scanner(System.in);
+        System.out.println("Input your name ");
+        String name = in.nextLine();
+        System.out.println("Input your age ");
+        int age = in.nextInt();
 
-        }
+        System.out.println("Your name is " + name);
+        System.out.println("Your age is " + age);
+        in.close();
+
+
     }
 }
